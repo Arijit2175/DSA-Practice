0ae2b5bd8055162d87b807ec71d6291fde21d97e
Arijit Karmakar <arijitkarmakar2175@gmail.com>
2026-07-24 11:39:28 +0530

    Time: 8 ms (98.04%) | Memory: 48.5 MB (47.86%) - LeetSync

--- 924-fair-candy-swap/fair-candy-swap.java
+++ 924-fair-candy-swap/fair-candy-swap.java
@@ -6,9 +6,7 @@ class Solution {
         int diff = (sumB - sumA) / 2;
         Set<Integer> set = new HashSet<>();
         for(int n : bobSizes) set.add(n);
-        for(int i = 0; i < aliceSizes.length; i++) {
-            if(set.contains(aliceSizes[i] + diff)) return new int[]{aliceSizes[i], aliceSizes[i] + diff};
-        }
+        for(int i = 0; i < aliceSizes.length; i++) if(set.contains(aliceSizes[i] + diff)) return new int[]{aliceSizes[i], aliceSizes[i] + diff};
         return new int[0];
     }
 }
